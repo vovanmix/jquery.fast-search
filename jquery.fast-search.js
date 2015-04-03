e65f505438638ba4dad07b17effcb97ef439b729
vladimir <singingcode@gmail.com>
2015-04-03 05:19:02 -0700

    minor bug fixes and style improvements

--- jquery.fast-search.js
+++ jquery.fast-search.js
@@ -17,41 +17,44 @@
  * limitations under the License.
  * ========================================================== */
 
+
 var fastSearch = {
     templateSimple: '<div class="search_row"><a href="%link%" class="item-name">%text%</a></div>',
     templateText: '<div class="search_row"><a href="%link%" class="item-name">%text%</a><span class="search-comment">%comment%</span></div>',
     templateWithImageSimple: '<div class="search_row"><div class="item-left"><a href="%link%" class="item-image"><img width="%width%" height="%height%" src="%image%"/></a></div><div class="item-info"><a href="%link%" class="item-name">%text%</a></div></div>',
     templateWithImage: '<div class="search_row"><div class="item-left"><a href="%link%" class="item-image"><img width="%width%" height="%height%" src="%image%"/></a></div><div class="item-info"><a href="%link%" class="item-name">%text%</a><span class="search-comment">%comment%</span></div></div>'
 };
-
 (function($) {
 
     $.fn.fastSearch = function(options) {
 
         var settings = $.extend({
             url: '/',
-			type: 'get',
-			dataType: 'html',
-			rowTemplate: fastSearch.templateSimple,
+            type: 'get',
+            dataType: 'html',
+            rowTemplate: fastSearch.templateSimple,
             onStart: function() {},
             onReady: function() {},
-			processResult: function(data){return data;},
-			processResultRow: function(dataRow){return dataRow;}
+            processResult: function(data){return data;},
+            processResultRow: function(dataRow){return dataRow;}
         }, options);
 
-		var render = function(data){
-			return template(settings.rowTemplate, data);
-		};
+        var render = function(data){
+            return template(settings.rowTemplate, data);
+        };
 
-		var template = function (template, data){
-			return template.replace(/%(\w*)%/g,function(m,key){return data.hasOwnProperty(key)?data[key]:"";});
-		};
+        var template = function (template, data){
+            if(data)
+                return template.replace(/%(\w*)%/g,function(m,key){return data.hasOwnProperty(key)?data[key]:"";});
+            else
+                return '';
+        };
 
         var search = function(e) {
 
             var text = $(this).val();
 
-			var fast_search_result = $(this).parent().find('.fast_search_result');
+            var fast_search_result = $(this).parent().find('.fast_search_result');
 
             if (text === '') {
                 fast_search_result.fadeOut('fast');
@@ -59,7 +62,7 @@ var fastSearch = {
             }
 
             settings.onStart(text);
-			
+
             fast_search_result.html('<div class="fast_search_loading"></div>');
             fast_search_result.fadeIn('fast');
 
@@ -73,61 +76,61 @@ var fastSearch = {
                 context: this,
                 success: function(data) {
 
-					var html = '';
-					if(settings.dataType == 'json'){
-						//settings.onRender(data);
+                    var html = '';
+                    if(settings.dataType == 'json'){
+                        //settings.onRender(data);
 
-						var convertedData = settings.processResult(data);
+                        var convertedData = settings.processResult(data);
 
-						for(var row in convertedData){
-							if(convertedData.hasOwnProperty(row)) {
-								var convertedDataRow = settings.processResultRow(convertedData[row]);
-								html += render(convertedDataRow);
-							}
-						}
+                        for(var row in convertedData){
+                            if(convertedData.hasOwnProperty(row)) {
+                                var convertedDataRow = settings.processResultRow(convertedData[row]);
+                                html += render(convertedDataRow);
+                            }
+                        }
 
-					}
-					else{
-						html = data;
-					}
+                    }
+                    else{
+                        html = data;
+                    }
 
 
                     if (html !== '') {
                         fast_search_result.html(html);
                     } else {
-						fast_search_result.html("");
+                        fast_search_result.html("");
                         fast_search_result.fadeOut('fast');
                     }
 
-					settings.onReady(html);
+                    settings.onReady(html);
 
                 }
             });
 
         };
 
-		var close = function(){
-			var fast_search_result = $(this).parent().find('.fast_search_result');
-			fast_search_result.fadeOut('fast');
-		};
+        var close = function(){
+            var fast_search_result = $(this).parent().find('.fast_search_result');
+            fast_search_result.fadeOut('fast');
+        };
 
-		var focus = function(){
-			var text = $(this).val();
-			if(text != ''){
-				$(this).trigger('keyup.fastSearch');
-			}
-		};
+        var focus = function(){
+            var text = $(this).val();
+            if(text != ''){
+                $(this).trigger('keyup.fastSearch');
+            }
+        };
 
         return this.each(function() {
-			if($(this).attr('fastSearch') == 'true'){
-				return;
-			}
+            if($(this).attr('fastSearch') == 'true'){
+                return;
+            }
             $(this).attr('autocomplete', 'off');
             $(this).attr('fastSearch', 'true');
             $(this).after('<div class="fast_search_result" style="display:none;"></div>');
             $(this).bind('keyup.fastSearch', search);
-			$(this).bind('blur.fastSearch', close);
-			$(this).bind('focus.fastSearch', focus);
+            $(this).bind('blur.fastSearch', close);
+            $(this).bind('focus.fastSearch', focus);
 
         });
 
